af2cf4362bf4924d892c1092084ea7e60f57a0d0
Rakato <ohubschmann@yahoo.com>
2017-05-04 15:30:25 -0400

    Update pima_logistic.py

--- pima_logistic.py
+++ pima_logistic.py
@@ -1,3 +1,5 @@
+
+
 import numpy as np
 import urllib
 # url with dataset
@@ -6,17 +8,19 @@ url = "http://archive.ics.uci.edu/ml/machine-learning-databases/pima-indians-dia
 raw_data = urllib.urlopen(url)
 # load the CSV file as a numpy matrix
 dataset = np.loadtxt(raw_data, delimiter=",")
-# separate the data from the target attributes
+
+#8th column is Diabetes yes/no indicator- what we are solving for
 X = dataset[:,0:8]
 y = dataset[:,8]
 
+#normalizing data
 from sklearn import preprocessing
 # normalize the data attributes
 normalized_X = preprocessing.normalize(X)
 # standardize the data attributes
 standardized_X = preprocessing.scale(X)
 
-
+#logistic regressioni and prediction 
 from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression()
