229943c426404b3d4a159ad5e73fb5f17ebb2791
Rakato <ohubschmann@yahoo.com>
2017-05-04 12:28:47 -0400

    Create kmeansmtcars.r

---
+++ kmeansmtcars.r
@@ -0,0 +1,24 @@
+
+
+#import mtcars dataset
+data("mtcars")
+
+#kmeans with "n" clusters
+fit <- kmeans(mtcars, 3) # pick number of clusters, eg "3"
+
+aggregate(mtcars, by=list(fit$cluster),FUN=mean)
+
+#make dataframe
+mtdf <- data.frame(mtcars, fit$cluster)
+
+#plot clusters
+plot(mtdf$fit.cluster )
+
+#Distance Matrix and Dendrogram
+d <- dist(mtdf, method = "euclidean") # distance matrix using euclidean
+fit <- hclust(d, method="ward") 
+plot(fit) # display dendogram
+groups <- cutree(fit, k=3) 
+# red borders around the "n" clusters 
+rect.hclust(fit, k=3, border="red")
+
